318a69f519b28a2fda018a588550633cb8b301c2
unknown <belasalah@gmail.com>
2025-01-16 12:01:05 +0100

    crud cars closed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 // config 
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, 'views'))
@@ -17,7 +18,8 @@ connection.connect((err)=>{
     console.log('database connected')
 })
 
-
+// middlewares
+app.use(bodyParser.urlencoded({extended: true}))
 // routes
 app.get('/cars' , (req, res)=>{
        connection.query("SELECT * FROM cars", (err, result)=>{
@@ -39,8 +41,59 @@ app.get('/car-show/:id' , (req, res)=>{
         })
        })
 })
+// ajouter un vehicule
+app.get('/add-car', (req, res)=>{ // localhost:3000/add-car [GET]
+    res.render('add-car', {
+        heading: "Ajouter un vehicule"
+    })
+})
+app.post('/add-car', (req, res)=>{
+    const {model, year,  image} = req.body
+    connection.query(`INSERT INTO cars (model, year, image) VALUES ('${model}', '${year}', '${image}')`, (err)=>{
+        if(err) throw err
+        res.redirect('/cars')
+    }) 
+       
+          
+})
+ 
+// update car 
+app.get('/update-car/:id', (req, res)=>{
+    const id = req.params.id
+    connection.query(`SELECT * FROM cars WHERE id =${id}`, (err, result)=>{
+        if(err) throw err
+        res.render('update-car', {
+            heading: "Modifier un vehicule",
+            car: result[0]
+        })
+    })
+})
+app.post('/update-car', (req, res)=>{
+    const {model, image, year , id} = req.body
+    connection.query(`UPDATE cars SET model = '${model}' , image = '${image}' , year = '${year}' WHERE id = ${id} `, (err, result)=>{
+        if(err) throw err
+        console.log(result)
+        res.redirect('/car-show/'+id)
+})
+})
 
+// delete car 
+app.post('/delete-car' , (req, res)=>{
+    const id = req.body.id
+    connection.query(`DELETE FROM cars WHERE id = ${id}`, (err)=>{
+        if(err) throw err
+        res.redirect('/cars')
+    })
+})
+// app.get('/delete-car/:id' , (req, res)=>{
+//     const id = req.params.id
+//     connection.query(`DELETE FROM cars WHERE id = ${id}`, (err)=>{
+//         if(err) throw err
+//         res.redirect('/cars')
+//     })
+// })
 
+// servercls
 app.listen(3000, ()=>{
     console.log('listening http://localhost:3000')
 })
